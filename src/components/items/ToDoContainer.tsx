7bfe67e088aa839685ff7324b047e328ae091c8a
Alberto Garrido <hello@albertogarrido.net>
2023-07-15 20:34:05 +0200

    add proper title when the tasks block is being populated with done tasks

--- src/components/items/ToDoContainer.tsx
+++ src/components/items/ToDoContainer.tsx
@@ -5,9 +5,15 @@ interface ToDoContainerProps {
 }
 
 const ToDoContainer = ({ tasks }: ToDoContainerProps) => {
+  let sectionTitle = "To Do List";
+
+  if (tasks.length > 0 && tasks[0].isDone === true) {
+    sectionTitle = "Tasks Completed";
+  }
+
   return (
     <div className="my-3 p-3 bg-body rounded shadow-sm">
-      <h6 className="border-bottom pb-2 mb-0">To Do List</h6>
+      <h6 className="border-bottom pb-2 mb-0">{sectionTitle}</h6>
       {tasks.length === 0 && <p>You dont have any tasks, enjoy your day!</p>}
       {tasks.map((todo, index) => (
         <div
